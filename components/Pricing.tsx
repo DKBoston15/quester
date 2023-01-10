ac55f0795b0a1e51ee30970bea4cd89bd75b31d3
Dakota Brown <dakotabrownag@gmail.com>
2023-01-10 00:07:05 -0700

    Test

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -71,7 +71,7 @@ export default function Pricing({ products }: Props) {
       <div className="max-w-6xl mx-auto py-8 sm:py-24 px-4 sm:px-6 lg:px-8">
         <div className="sm:flex sm:flex-col sm:align-center">
           <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-            Pricing Plans
+            Pricing Plans TEST
           </h1>
           <p className="mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl max-w-2xl m-auto">
             Start building for free, then add a site plan to go live. Account
